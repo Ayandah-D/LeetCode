c409e21ceb20c507c201a1cfcce51b8be148eba8
Ayandah-D <87431114+Ayandah-D@users.noreply.github.com>
2025-01-20 16:53:28 +0200

    leet01

---
+++ 02/class Solution0 {.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+	int maxWealthSofar = 0;
+
+	for (int[]customer : accounts){
+		int currentCustomerWealth = 0;
+		
+		for (int bank : customer) { 
+		currentCustomerWealth += bank;
+}
+maxWealthSofar = Math.max(maxWealthSofar, currentCustomerWealth);
+}
+return maxWealthSofar;
+
+
+        
+    }
+}
